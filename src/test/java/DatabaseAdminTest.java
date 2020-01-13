6176b629d4e05a35a0b319f9094e8e3a0dd8612d
Nelson <nbritob@hotmail.com>
2020-01-13 14:24:33 +0000

    adds tests for database admin

---
+++ src/test/java/DatabaseAdminTest.java
@@ -0,0 +1,42 @@
+import org.junit.Before;
+import org.junit.Test;
+import staff.techStaff.*;
+
+import static org.junit.Assert.assertEquals;
+
+public class DatabaseAdminTest {
+
+    DatabaseAdmin databaseAdmin;
+
+    @Before
+    public void before() {
+        databaseAdmin = new DatabaseAdmin("Sandy", "JX404A", 1000.00);
+    }
+
+    @Test
+    public void canGetName() {
+        assertEquals("Sandy", databaseAdmin.getName());
+    }
+
+    @Test
+    public void canGetNationalInsuranceNumber() {
+        assertEquals("JX404A", databaseAdmin.getNationalInsuranceNumber());
+    }
+
+    @Test
+    public void canGetSalary() {
+        assertEquals(1000.00, databaseAdmin.getSalary(), 0.01);
+    }
+
+    @Test
+    public void canRaiseSalary() {
+        databaseAdmin.raiseSalary(2.00);
+        assertEquals(1002, databaseAdmin.getSalary(), 0.01);
+    }
+
+    @Test
+    public void canGetPayBonus() {
+        databaseAdmin.payBonus();
+        assertEquals(10, databaseAdmin.payBonus(), 0.01);
+    }
+}
